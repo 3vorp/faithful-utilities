477bb2f0cf7878054381e8fee1d7a456a128a6b0
Evorp <3vorpgaming@gmail.com>
2023-12-21 22:35:15 -0800

    add existing tools

---
+++ createChangelog.js
@@ -0,0 +1,120 @@
+/**
+ * Generate a basic formatted changelog for a specified pack after a given date.
+ * @author Evorp
+ */
+
+const { writeFileSync } = require("fs");
+const rl = require("readline").createInterface(process.stdin, process.stdout);
+const prompt = (query) => new Promise((resolve) => rl.question(query, resolve));
+
+// will take some time
+const mapUsernames = () =>
+    fetch("https://api.faithfulpack.net/v2/contributions/authors")
+        .then((res) => res.json())
+        .then((d) => d.reduce((acc, cur) => ({ ...acc, [cur.id]: cur.username }), {}));
+
+const toSentenceCase = (str) =>
+    str
+        .split("_")
+        .map((word) => word[0].toUpperCase() + word.slice(1))
+        .join(" ");
+
+async function getPacks() {
+    const res = await fetch("https://api.faithfulpack.net/v2/settings/submission.packs");
+    const allPacks = await res.json();
+    return Object.entries(allPacks).map(([k, v]) => ({ id: k, name: v.display_name }));
+}
+
+/**
+ * Prompt the user for a date and pack, and generate a changelog based off the information given and write it to a text file.
+ * @author Evorp
+ */
+async function createChangelog() {
+    const IDtoUsername = await mapUsernames();
+    const YMD = await prompt("Please give the date since the previous changelog (YYYY-MM-DD): ");
+    const res = await fetch(
+        `https://api.faithfulpack.net/v2/contributions/between/${new Date(
+            YMD
+        ).getTime()}/${new Date().getTime()}`
+    );
+    const allContributions = await res.json();
+    const packs = await getPacks();
+
+    // choice menu is easier than manually entering in pack ids
+    const packIndex = await prompt(
+        `Please provide the pack to create a changelog for:\n\t${packs
+            .map((v, i) => `${i + 1}) ${v.name}`)
+            .join("\n\t")}\nChoose which pack you want using the corresponding number: `
+    );
+
+    console.log(
+        "Collecting data...\nThis can take some time for big changelogs, please be patient..."
+    );
+
+    // remove unnecessary info to save time later on
+    const packContributions = allContributions
+        .filter((contribution) => contribution.pack == packs[packIndex - 1].id)
+        .map((contribution) => ({
+            id: contribution.texture,
+            authors: contribution.authors.map((author) => IDtoUsername[author] ?? "Anonymous"),
+        }));
+
+    // group ids into 30-long nested arrays
+    const groupedIDs = packContributions
+        .map((contribution) => contribution.id)
+        .reduce((acc, cur, index) => {
+            if (index % 30 === 0) acc.push([]);
+            acc.at(-1).push(cur);
+            return acc;
+        }, []);
+
+    const textureData = (
+        await Promise.all(
+            groupedIDs.map(async (ids) => {
+                // get texture data in batches of 30
+                return await fetch(`https://api.faithfulpack.net/v2/textures/${ids.join(",")}`)
+                    .then((res) => res.json())
+                    .catch(() => null);
+            })
+        )
+    )
+        .flat()
+        .map((texture) => ({
+            ...texture,
+            tags: texture.tags.filter((tag) => !["java", "bedrock"].includes(tag.toLowerCase())),
+        }));
+
+    // merge the two objects by id
+    const finalData = packContributions.map((contribution) => ({
+        ...contribution,
+        ...textureData.find((texture) => texture.id == contribution.id),
+    }));
+
+    // group by texture tag (easier than going off paths)
+    const formatted = finalData.reduce(
+        (acc, texture) => ({
+            ...acc,
+            [texture.tags[0]]: [
+                ...(acc[texture.tags[0]] ?? []),
+                `${toSentenceCase(texture.name)} (${texture.authors.join(", ")})`,
+            ],
+        }),
+        {}
+    );
+
+    console.log(formatted);
+
+    writeFileSync("./changelog.json", JSON.stringify(formatted, null, 4));
+    writeFileSync(
+        "./changelog.md",
+        Object.entries(formatted)
+            .map(([k, v]) => `## ${k}\n- ${v.join("\n- ")}`)
+            .join("\n\n")
+    );
+    console.log(
+        "Written changelog file to ./changelog.json and ./changelog.md!\nRemember this is not directly compatible with website posts, use a JSON to YAML parser like https://json2yaml.com/ for that."
+    );
+    process.exit();
+}
+
+createChangelog();
---
+++ createContributions.js
@@ -0,0 +1,131 @@
+/**
+ * Create either a contribution or a series of contributions for any Faithful pack using a CLI interface
+ * @author Evorp
+ */
+
+const FAITHFUL_API_TOKEN = "YOUR_TOKEN_HERE";
+
+const CHAIN_CONTRIBUTIONS = false; // after the first contribution reuse the same date and authors until you're done
+const DEV = false; // turn this off to actually enable writing
+
+const PACK = "classic_faithful_32x";
+const RESOLUTION = 32;
+
+const rl = require("readline").createInterface({ input: process.stdin, output: process.stdout });
+const prompt = (query) => new Promise((resolve) => rl.question(query, resolve));
+
+// will take some time
+const mapUsernames = () =>
+	fetch("https://api.faithfulpack.net/v2/users/names")
+		.then((res) => res.json())
+		.then((d) =>
+			d.reduce((acc, cur) => ({ ...acc, [cur.username?.toLowerCase()]: cur.id }), {})
+		);
+
+/**
+ * Prompt the user for a texture, give a choice menu if necessary, and return the texture ID
+ * @author Evorp
+ * @returns texture ID
+ */
+async function getTextures() {
+	const textureName = await prompt("Give the name of the texture: ");
+	const res = await fetch(`https://api.faithfulpack.net/v2/textures/${textureName}/all`);
+	let textures = await res.json();
+
+	if (!textures.length) return console.log("That texture doesn't exist!\n\nRestarting...");
+
+	if (textures.length > 1) {
+		const i = await prompt(
+			`There are multiple textures: \n\t${textures
+				.map((t, i) => `${i + 1}) [#${t.id}] ${t.paths[0].name}`)
+				.join(
+					"\n\t"
+				)}\nChoose which texture you want using the corresponding number or range (inclusive): `
+		);
+
+		if (i.includes("-")) {
+			const [min, max] = i.split("-");
+			textures = textures.slice(min - 1, max);
+		} else textures = [textures[i - 1]];
+	}
+
+	return textures.map((t) => t.id);
+}
+
+/**
+ * Prompt the user for a series of usernames and append them to an array
+ * @returns array of usernames
+ */
+async function getUsernames() {
+	const usernames = [];
+	while (true) {
+		const username = await prompt('Enter a username (use "DONE" to move to the next step): ');
+		if (username == "DONE") break;
+		usernames.push(username.toLowerCase());
+	}
+	return usernames;
+}
+
+async function createContributions(previousContributions = []) {
+	// initialize base with previous author and date if possible
+	const base = previousContributions.length
+		? { date: previousContributions[0].date, authors: previousContributions[0].authors }
+		: {};
+
+	if (!previousContributions.length) {
+		const date = await prompt("Give the contribution date in the format YYYY-MM-DD: ");
+
+		const usernames = await getUsernames();
+		const usernameToID = await mapUsernames();
+
+		base.date = new Date(date).getTime() + 82800000;
+		if (!base.date) console.error(`${date} isn't a valid date!`);
+		base.authors = usernames
+			.map((u) => usernameToID[u] ?? console.error(`No user called "${u}" was found!`))
+			.filter((v) => v);
+	}
+
+	const textures = await getTextures();
+	if (!textures) return await createContributions(previousContributions);
+
+	const contributions = [
+		...previousContributions,
+		...textures.map((texture) => ({ ...base, texture, pack: PACK, resolution: RESOLUTION })),
+	];
+
+	if (CHAIN_CONTRIBUTIONS) {
+		// only write when the user is ready to, otherwise accumulate contributions
+		const confirm = await prompt(
+			"Are you finished? Make sure this data looks correct before writing it [Y/N]: "
+		);
+		if (confirm.toLowerCase() !== "y") {
+			console.log("Starting new contribution...\n\n");
+			// continue adding contributions to same array if not done (recursive)
+			return await createContributions(contributions);
+		}
+	}
+
+	// don't write when in dev mode
+	if (DEV) return await createContributions(CHAIN_CONTRIBUTIONS ? contributions : []);
+
+	// either no chaining or chaining finished
+	return fetch("https://api.faithfulpack.net/v2/contributions", {
+		method: "post",
+		headers: {
+			bot: FAITHFUL_API_TOKEN,
+			// stupid fetch api stuff, I wish I was using axios here lol
+			Accept: "application/json",
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(contributions),
+	}).then(async (res) => {
+		const data = await res.json();
+		if (!res.ok) return console.error(`An error occurred: ${JSON.stringify(data)}`);
+
+		console.log(data);
+		// start again from scratch now that chaining that particular one has stopped
+		return await createContributions();
+	});
+}
+
+createContributions();
